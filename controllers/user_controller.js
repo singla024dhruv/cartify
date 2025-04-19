a0f6e6e92ba69da64d5088264c8262ab1f0c1ff3
Dhruv <dhruvsingla024@gmail.com>
2025-04-20 01:54:09 +0530

    some errors resolved

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -2,12 +2,12 @@ const User = require('../models/user');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 module.exports.SignUp = async function (req, res) {
-    
-    const { name, email, password } = req.body;
+    console.log(req.body);
+    const { username, email, password } = req.body;
     try {
         const saltrounds = 10;
         const hashedPassword = await bcrypt.hash(password, saltrounds);
-        const newUser = new User({ name, email, password: hashedPassword });
+        const newUser = new User({ name: username, email: email, password: hashedPassword });
         await newUser.save();
         const token = jwt.sign({
              id: newUser._id.toString()
@@ -27,7 +27,7 @@ module.exports.SignUp = async function (req, res) {
         console.log(`${username} is logged in successfully`);
     }
     catch (err) {
-        console.error(error);
+        console.error(err);
         res.status(500).send('An error occurred');
     }
 };
