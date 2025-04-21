a08aacd3ee449be59573088c16b857c93079d5a5
Dhruv <dhruvsingla024@gmail.com>
2025-04-21 15:55:48 +0530

    logged in successfully

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -10,24 +10,57 @@ module.exports.SignUp = async function (req, res) {
         const newUser = new User({ name: username, email: email, password: hashedPassword });
         await newUser.save();
         const token = jwt.sign({
-             id: newUser._id.toString()
+            id: newUser._id.toString()
         }, process.env.JWT_SECRET,
-            {expiresIn:'3d'}
+            { expiresIn: '3d' }
         );
         //res.status(201).send('User registerd successfully');
-          res.status(201).json({
+        res.status(201).json({
             message: "User registered successfully",
             token,
             user: {
-             id: newUser._id.toString(),
-              username: newUser.username,
-              email: newUser.email,
+                id: newUser._id.toString(),
+                username: newUser.name,
+                email: newUser.email,
             },
-          });
-        console.log(`${username} is logged in successfully`);
+        });
+        console.log(`${username} is registered successfully`);
     }
     catch (err) {
         console.error(err);
         res.status(500).send('An error occurred');
+    }
+}
+    module.exports.Login = async function (req, res) {
+        console.log("hello");
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email: email });
+        if (user) {
+            const ans = await bcrypt.compare(password, user.password);
+            if (ans) {
+                const token = jwt.sign(
+                    {
+                        id: user._id.toString(),
+                    },
+                    process.env.JWT_SECRET,
+                    { expiresIn: "3d" }
+                );
+                res.status(201).json({
+                    message: "User Logged in successfully",
+                    token,
+                    user: {
+                        id: user._id.toString(),
+                        username: user.name,
+                        email: user.email,
+                    },
+                });
+                console.log(`${user} us logged in successfully`);
+            }
+        }
+    }
+    catch (err)
+    {
+        console.log(err);
     }
 };
--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,5 @@ const router = express.Router();
 console.log("router is loaded");
 const user_controller = require('../controllers/user_controller');
 router.post('/register', user_controller.SignUp);
+router.post('/login', user_controller.Login);
 module.exports = router;
